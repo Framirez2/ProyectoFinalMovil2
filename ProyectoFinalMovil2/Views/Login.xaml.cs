d42bcf778edde39b83b49e2f74ea907085ed8d04
Framirez2 <116310432+Framirez2@users.noreply.github.com>
2023-06-27 23:02:54 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoFinalMovil2/Views/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalMovil2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void btnlogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Profile());
+        }
+        private async void btnregistrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
