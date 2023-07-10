35f963c821b014d089c44eecb58ed99728815ecf
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-07-09 21:56:21 -0600

    Modelo usuarios

---
+++ ProyectoFinalMovil2/Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMovil2.Models
+{
+    public class Usuarios
+    {
+        public string Id_User { get; set; }
+        public string Id_User_Cliente { get; set; }
+
+        public string Nombres { get; set; }
+
+
+    }
+}
