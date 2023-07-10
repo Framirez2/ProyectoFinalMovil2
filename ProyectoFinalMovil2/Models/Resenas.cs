bb8096460a0817d97d7641acf8e9ed0d556ba174
Framirez2 <116310432+Framirez2@users.noreply.github.com>
2023-07-09 22:29:47 -0600

    Modelo Reseñas

---
+++ ProyectoFinalMovil2/Models/Resenas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMovil2.Models
+{
+    class Resenas
+    {
+        public string Id_Resena { get; set; }
+        public string Id_Reservacion { get; set; }
+        public string Calificacion { get; set; }
+        public string Id_Usuario { get; set; }
+        public string Resena { get; set; }
+    }
+}
