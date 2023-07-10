9da456c4e9e62d0784f245ceb11406fe1f248e3d
Framirez2 <116310432+Framirez2@users.noreply.github.com>
2023-07-09 22:21:17 -0600

    Trabajadores

---
+++ ProyectoFinalMovil2/Models/Trabajadores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinalMovil2.Models
+{
+    public class Trabajadores
+    {
+        public string id_t { get; set; }
+        public string Nombres { get; set; }
+        public string Correo { get; set; }
+        public string Contr { get; set; }
+        public string Icono { get; set; }
+        public string tipo_User { get; set; }
+    }
+}
