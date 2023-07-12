6b682b8e1b9433d1fbbef7e9e64db2121b12ce08
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-07-11 18:43:21 -0600

    11/7/2023

--- ProyectoFinalMovil2/Models/HorasReservadas.cs
+++ ProyectoFinalMovil2/Models/HorasReservadas.cs
@@ -7,6 +7,7 @@ namespace ProyectoFinalMovil2.Models
     public class HorasReservadas
     {
         public string Nombre_UsuarioReserv { get; set; }
+
         public string Nombre_Estilista { get; set; }
 
         public string Hora_Reservacion { get; set; }
