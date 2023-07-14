093e4b9321aff77890dcb8c7c1a389b73f13c9da
AvailableNameWill <carloswill06@gmail.com>
2023-07-14 12:15:31 -0600

    Listas
    listas

---
+++ ProyectoFinalMovil2/Views/FinalizarReservaciones.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalMovil2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FinalizarReservaciones : ContentPage
+    {
+        public FinalizarReservaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFinalizarRes_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listReservaciones_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+    }
+}
