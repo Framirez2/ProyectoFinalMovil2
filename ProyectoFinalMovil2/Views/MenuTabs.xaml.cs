5a653a114373393e66a99b673cc814eee7bddc30
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-07-13 20:04:49 -0600

    13/72023

--- ProyectoFinalMovil2/Views/MenuTabs.xaml.cs
+++ ProyectoFinalMovil2/Views/MenuTabs.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace ProyectoFinalMovil2.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PageReservacion : ContentPage
+    public partial class MenuTabs : TabbedPage
     {
-        public PageReservacion()
+        public MenuTabs ()
         {
             InitializeComponent();
         }
