123dd67e825f3765cad1b980c4b60d8494365910
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-07-20 12:42:50 -0600

    Cambios

--- ProyectoFinalMovil2/Views/CategoriesServices.xaml.cs
+++ ProyectoFinalMovil2/Views/CategoriesServices.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ProyectoFinalMovil2.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,10 @@ namespace ProyectoFinalMovil2.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CategoriesServices : ContentPage
 	{
-		public CategoriesServices ()
+        
+        Usuarios Datos_Usuario = new Usuarios();
+        string Id_Usuario;
+        public CategoriesServices ()
 		{
 			InitializeComponent ();
 		}
@@ -22,9 +27,13 @@ namespace ProyectoFinalMovil2.Views
 
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private async void Button_Clicked_1(object sender, EventArgs e)
         {
-
+            string[] descripcion = new string[3];
+            descripcion[0] = "- Limado de Pies.";
+            descripcion[1] = "- Corte de Uñas.";
+            descripcion[2] = "- Exfoliación para Retirar las Celulas Muertas.";
+            await Navigation.PushAsync(new Reservaciones(Id_Usuario, 1, "Pedicure", Datos_Usuario, "150", descripcion));
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
---
+++ ProyectoFinalMovil2/Views/Reservaciones.xaml.cs
@@ -0,0 +1,100 @@
+﻿using ProyectoFinalMovil2.Controllers;
+using ProyectoFinalMovil2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinalMovil2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reservaciones : ContentPage
+    {
+        private string IdUsuario, Precio, FechaReservacion, NombreEstilista = "-", NombreUsuario, HoraReservacion, TipoReservacion;
+        private string[] ImageService = { "manicure", "pedicure", "retoquemanos", "uacrilicas" };
+        private string[] Servicio = { "Manicure", "Pedicure", "Retoque Acrílico", "Colocación y pintado de uñas acrílicas" };
+        private int ContadorEstilista, ContadorReservaciones;
+
+        private Usuarios Data;
+        public Reservaciones(string idUsuario, int Service, string Reservacion, Usuarios data, string Precio, string[] Descripcion)
+        {
+            InitializeComponent();
+            MostrarEstilistas();
+            this.IdUsuario = idUsuario;
+            this.TipoReservacion = Reservacion;
+            this.Data = data;
+            this.Precio = Precio;
+            if (this.Data.Tipo_Usuario == "Cliente")
+            {
+                txtPrecio.IsEnabled = false;
+            }
+            else if (this.Data.Tipo_Usuario == "admin")
+            {
+                txtPrecio.IsEnabled = true;
+            }
+            txtServicio.Text = Servicio[Service];
+            txtDesc1.Text = Descripcion[0];
+            txtDesc2.Text = Descripcion[1];
+            txtDesc3.Text = Descripcion[2];
+            txtPrecio.Text = "L. " + Precio;
+            ImagenServicio(Reservacion);
+        }
+
+        private void ImagenServicio(string Servicio)
+        {
+            if (Servicio.Equals("Manicure"))
+            {
+                txtImgServicio.Source = ImageService[0];
+            }
+            else if (Servicio.Equals("Pedicure"))
+            {
+                txtImgServicio.Source = ImageService[1];
+            }
+            else if (Servicio.Equals("AplicaciondeAcrilico"))
+            {
+                txtImgServicio.Source = ImageService[3];
+            }
+            else if (Servicio.Equals("PintadoYDecoracion"))
+            {
+                txtImgServicio.Source = ImageService[2];
+            }
+        }
+
+        private void txtListaEstilista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtFechaReservacion_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void txtHorario_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtReservar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFechaReservacion_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
+
+        private async Task MostrarEstilistas()
+        {
+            var funcion = new ContM_Categorias();
+            var data = await funcion.ObtenerEstilistas();
+            txtListaEstilista.ItemsSource = data;
+        }
+    }
+}
