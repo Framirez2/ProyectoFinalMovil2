816aee942c0c03f5c44b5ca04a5e74c67d6695b2
LeninSalinas <lenin.salinas77@gmail.com>
2023-07-31 23:52:28 -0600

    Restablecer contra a un 90%

--- ProyectoFinalMovil2/Views/Recuperar.xaml.cs
+++ ProyectoFinalMovil2/Views/Recuperar.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFinalMovil2.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,38 @@ namespace ProyectoFinalMovil2.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Recuperar : ContentPage
 	{
+		Cont_RestablecerCuenta _RestablecerCuenta=new Cont_RestablecerCuenta();
 		public Recuperar ()
 		{
 			InitializeComponent ();
 		}
-	}
+        private async void btnRecuperar_Clicked(object sender, EventArgs e)
+        {
+            string correo = txtCorreo.Text;
+            try
+            {
+
+            }catch (Exception ex)
+            {
+                await DisplayAlert(null, ex.Message, "Aceptar");
+            }
+
+            if (string.IsNullOrEmpty(correo))
+            {
+                await DisplayAlert(null, "Porfavor ingrese su correo","Aceptar");
+                return;
+            }
+
+            bool seEnvio = await _RestablecerCuenta.RestablecerClvae(correo);
+            if (seEnvio)
+            {
+                await DisplayAlert(null, "Revise su correo electronico", "Ok");
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                await DisplayAlert("Error", "Correo no valido o no se pudo enviar el correo", "Aceptar");
+            }
+        }
+    }
 }
