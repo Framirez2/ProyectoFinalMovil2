b13d74a47683ebef78e0acacc7f2214233df5869
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-08-12 16:22:16 -0600

    Login Cambios de apariencia

---
+++ ProyectoFinalMovil2.Android/CustomRenderer/MyEntryRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ProyectoFinalMovil2.Custom;
+using ProyectoFinalMovil2.Droid.CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+[assembly: ExportRenderer(typeof(MyCustomEntry), typeof(MyEntryRenderer))]
+namespace ProyectoFinalMovil2.Droid.CustomRenderer
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+
+    }
+}
---
+++ ProyectoFinalMovil2/Custom/MyCustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProyectoFinalMovil2.Custom
+{
+    public class MyCustomEntry: Entry
+    {
+
+    }
+}
