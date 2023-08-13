dbf2cd659318ea7190a982e0cf0ab00b7073be57
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-08-12 20:13:58 -0600

    Cambios Interfaz

--- ProyectoFinalMovil2/Views/NavAdminDetail.xaml.cs
+++ ProyectoFinalMovil2/Views/NavAdminDetail.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using Firebase.Auth;
+using Newtonsoft.Json;
+using Plugin.Media.Abstractions;
+using ProyectoFinalMovil2.Controllers;
+using ProyectoFinalMovil2.Models;
+using ProyectoFinalMovil2.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +18,63 @@ namespace ProyectoFinalMovil2.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NavAdminDetail : ContentPage
     {
+        MediaFile file;
+        string rutafoto;
+        string Iduserlogin;
+        string pass;
+        string IdUsuariosClientes;
+        string estado = "vacio";
+        string nombre;
+        string tipoUser;
+        string nombreEdit;
+
         public NavAdminDetail()
         {
             InitializeComponent();
+            ObtenerDatoUsuario();
+        }
+
+
+        private async Task ObtenerDatoUsuario()
+        {
+            ContM_Usuarios funcion = new ContM_Usuarios();
+            Usuarios parametros = new Usuarios();
+            parametros.Id_User = Iduserlogin;
+            var dt = await funcion.GetDataProfile(parametros);
+            foreach (var fila in dt)
+            {
+                nombres.Text = fila.Nombres;
+                btnsele.Source = fila.ImagenPerfil;
+                pass = fila.Contrasena;
+                rutafoto = fila.ImagenPerfil;
+                IdUsuariosClientes = fila.Id_User_Cliente;
+                nombre = fila.Nombres;
+            }
+
+        }
+
+        private async Task ObtenerIdusuario()
+        {
+            try
+            {
+                var authProvider = new FirebaseAuthProvider(new FirebaseConfig(FirebaseConnection.Clave_APIweb));
+                //validar si el usuario se ha validado o no dentro de la aplicacion
+                var guardarId = JsonConvert.DeserializeObject<FirebaseAuth>(Preferences.Get("MyFirebaseRefreshToken", ""));
+                //var RefrescarContenido = await authProvider.RefreshAuthAsync(guardarId);
+                //Preferences.Set("MyFirebaseRefreshToken", JsonConvert.SerializeObject(RefrescarContenido));
+                //el ID
+
+                Iduserlogin = guardarId.User.LocalId;
+
+                await ObtenerDatoUsuario();
+            }
+            catch (Exception)
+            {
+
+                await DisplayAlert("Alerta", "Sesion expirada", "OK");
+            }
+
+
         }
 
         /*private void Button_Clicked(object sender, EventArgs e)
